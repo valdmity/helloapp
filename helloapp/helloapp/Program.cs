8c9c6b7127dab93a19d6d5c5c57e1d45c2fc0be3
VladSurov <vlsur@outlook.com>
2021-10-30 10:23:30 +0500

    Initial commit

---
+++ helloapp/helloapp/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace helloapp
+{
+    internal class Program
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("hello");
+        }
+    }
+}
