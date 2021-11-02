3a6dcef15194a6a1f13c98f6ba4befbd6f0b53e9
VladSurov <vlsur@outlook.com>
2021-11-02 22:16:13 +0500

    hello white

--- helloapp/helloapp/Program.cs
+++ helloapp/helloapp/Program.cs
@@ -4,6 +4,11 @@ namespace helloapp
 {
     internal class Program
     {
+        public static void GreetWhite()
+        {
+            Console.WriteLine("hello white");
+        }
+        
         public static void Main(string[] args)
         {
             Console.WriteLine("hello");
